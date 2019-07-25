11f6b0e155e242ea97dc1130908a015acc277b3c
Weiyang Wang <wew168@.ucsd.edu>
2019-07-24 17:35:15 -0700

    server

--- req_res/server.py
+++ req_res/server.py
@@ -2,7 +2,7 @@ import random
 from sanic import Sanic
 from sanic.response import json, text
 
-texts=[text('a'*(500*(2**i))) for i in range(11)]
+texts=[text('a'*(500*(2**i))) for i in range(10)]
 #texts=['a'*1000000]
 
 app = Sanic()
@@ -12,4 +12,4 @@ async def test(request):
     return random.choice(texts)
 
 if __name__ == "__main__":
-    app.run(host='10.0.1.6', port=8000, workers=8)
+    app.run(host='10.0.1.6', port=8000, workers=8, access_log=False)
